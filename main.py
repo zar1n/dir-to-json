3878c0955417b53f192109492b744eeb2b4c0c2c
Zar1n <zarinsad@gmail.com>
2014-07-09 19:49:55 +1100

    Many fixes

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import hashlib
 
 class Dirtojson:
 
-    excludeFile = "exclude.txt"
+    configExclude       = "exclude.txt"
+    configExportJson    = "export.json"
 
     def walkdir(self, path):
         dirJson = []
@@ -17,25 +18,30 @@ class Dirtojson:
 
         fileEx = self.exclude()
 
-        for dirRoot, dirDir, dirFile in os.walk(path):
+        for dirRoot, dirDir, dirFile in os.walk(path, topdown=True):
+            dirDir[:] = [d for d in dirDir if d not in fileEx]
             for file in dirFile:
                 filePath = "%s/%s" % (dirRoot, file)
                 fileHash = hashlib.md5(filePath).hexdigest()
 
                 if file not in fileEx:
-                    if dirDir:
-                        filesJson.append({'name': file, 'directory': dirDir[0], 'hash': fileHash})
+                    if os.path.basename(dirRoot):
+                        filesJson.append({'name': file, 'directory': os.path.basename(dirRoot), 'hash': fileHash})
                     else:
                         filesJson.append({'name': file, 'hash': fileHash})
 
-        print json.dumps(dirJson, indent=2, ensure_ascii=False)
+        self.writeToFile(json.dumps(dirJson, indent=2, ensure_ascii=False))
 
     def exclude(self):
         exList = []
-        exDesc = open(self.excludeFile, 'rb')
+        exDesc = open(self.configExclude, 'rb')
         for exLine in exDesc:
             exList.append(exLine.rstrip('\n'))
         return exList
 
+    def writeToFile(self, jsonDumps):
+        exportFileDesc = open(self.configExportJson, 'wb')
+        exportFileDesc.write(jsonDumps)
+
 main = Dirtojson()
-main.walkdir("J:\@Movie")
+main.walkdir("J:\@Movie\\")
